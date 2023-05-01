f3fa9e11dd1054fab9292b2a9f572270a4f9f684
karlayventura <kaarlayventura@gmail.com>
2023-05-01 18:17:15 -0400

    Solar System Basic Project Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+       SolarSystem factorX = new SolarSystem();
+        factorX.sun="1";
+        factorX.moon="4";
+        factorX.planet="2";
+        factorX.star="10000";
+
+    }
+}
